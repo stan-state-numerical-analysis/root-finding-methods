6b7158487045e1b4eb2a9daee22a24736167be67
Jessica De Silva <74026509+jessicadesilva@users.noreply.github.com>
2024-08-09 16:06:43 -0700

    Create task_3.py

---
+++ homework/task_3.py
@@ -0,0 +1,18 @@
+# Task: Apply Newton's Method to find extreme values and visualize the output.
+
+# 1. Import the matplotlib.pyplot, numpy, the newtons_method function from the task_1.py file, and the central_difference
+# function from the task_2.py file.
+
+
+
+# 2. Define a new function called plot_extreme_value that applies Newton's method to find an extreme value
+#    of any function and plots the results.
+#    - Parameters:
+#      - f: The function whose derivative is being analyzed.
+#      - x_interval: A tuple specifying the interval of x values containing the extreme value.
+
+
+
+  # Copy and paste the code from plot_newtons_method from the task_2.py file and make the necessary
+  # changes to find and plot an extreme value of f on the interval x_interval (and plots f itself).
+  # Make the title of the plot: f"Approximation of an Extreme Value of f(x) in [{x_values[0]}, {x_values[1]}]"
